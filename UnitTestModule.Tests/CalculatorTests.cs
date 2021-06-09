c5796394ded6d16ffe1cda56cdebea5e1de5b1e3
sergii <servasylenko@deloitte.com>
2021-06-09 09:43:57 +0300

    Add project files.

---
+++ UnitTestModule.Tests/CalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestModule.Tests
+{
+    [TestFixture]
+    public class CalculatorTests 
+    {
+        private readonly ICalculator calculator;
+
+        public CalculatorTests()
+        {
+            this.calculator = new Calculator();
+        }
+
+        [Test]
+        public void AdditionalShouldTrue()
+        {
+            var result = calculator.Additional(3, 2);
+            Assert.AreEqual(5, result);
+            Assert.AreNotEqual(6, result);
+        }
+
+        [Test]
+        public void SubtractionShouldTrue()
+        {
+            var result = calculator.Subtraction(3, 2);
+            Assert.AreEqual(1, result);
+            Assert.AreNotEqual(2, result);
+        }
+
+        [Test]
+        public void MiltiplicationShouldTrue()
+        {
+            var result = calculator.Miltiplication(3, 2);
+            Assert.AreEqual(6, result);
+            Assert.AreNotEqual(5, result);
+        }
+
+        [Test]
+        public void DivisionShouldTrue()
+        {
+            var result = calculator.Division(6, 2);
+            Assert.AreEqual(3, result);
+            Assert.AreNotEqual(5, result);
+        }
+
+        [Test]
+        public void DivisionDivideByZeroShouldThrowException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(6, 0));
+        }
+    }
+}
---
+++ UnitTestModule/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestModule
+{
+    public interface ICalculator
+    {
+        int Additional(int a, int b);
+        int Division(int a, int b);
+        int Miltiplication(int a, int b);
+        int Subtraction(int a, int b);
+    }
+}
